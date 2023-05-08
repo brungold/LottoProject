2a927036e5e4ceb8473a69f66ee8f2231b52931d
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-05-08 22:50:56 +0200

    added an object and used the start method on it

--- src/main/java/pl/lotto/Main.java
+++ src/main/java/pl/lotto/Main.java
@@ -1,7 +1,10 @@
 package pl.lotto;
 
+import pl.lotto.engine.ApplicationEngine;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Lotto");
+        ApplicationEngine engine = new ApplicationEngine();
+        engine.start();
     }
 }
