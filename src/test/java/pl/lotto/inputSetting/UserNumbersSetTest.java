8e9b64dfc10984c272caeaa8a5c2ccd3152248b2
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-05-13 12:19:41 +0200

    added drafts/ or rather ideas of names tests cases to the class UserNumbersSetTest

--- src/test/java/pl/lotto/inputSetting/UserNumbersSetTest.java
+++ src/test/java/pl/lotto/inputSetting/UserNumbersSetTest.java
@@ -6,15 +6,18 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserNumbersSetTest implements SampleScanner{
+class UserNumbersSetTest implements SampleScanner {
 
     @Test
     public void testCollectNumbers() {
+        // given
         String inputData = "1\n2\n3\n4\n5\n6\n";
         UserNumbersSet userNumbersSet = createUserNumbersSet(inputData);
 
+        // when
         Set<Integer> selectedNumbers = userNumbersSet.collectNumbers();
 
+        // then
         assertEquals(6, selectedNumbers.size());
         assertTrue(selectedNumbers.contains(1));
         assertTrue(selectedNumbers.contains(2));
@@ -22,5 +25,47 @@ class UserNumbersSetTest implements SampleScanner{
         assertTrue(selectedNumbers.contains(4));
         assertTrue(selectedNumbers.contains(5));
         assertTrue(selectedNumbers.contains(6));
+    }
+
+    @Test
+    public void collect_numbers_returns_correct_set_of_numbers_when_user_inputs_valid_data() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void collect_numbers_returns_error_when_user_inputs_less_than_6_numbers() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void collect_numbers_returns_error_when_user_inputs_more_than_6_numbers() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void collect_numbers_returns_error_when_user_inputs_number_outside_1_to_99_range() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void collect_numbers_returns_error_when_user_inputs_duplicate_number() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void collect_numbers_returns_correct_set_of_numbers_when_user_inputs_both_valid_and_invalid_data() {
+        //given
+        //when
+        //then
     }
 }
