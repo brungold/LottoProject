e20d7695779ceebf731946b9e803600d155135ec
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-05-15 12:19:28 +0200

    refactored test cases of the class ApplicationEngine

--- src/test/java/pl/lotto/engine/ApplicationEngineTest.java
+++ src/test/java/pl/lotto/engine/ApplicationEngineTest.java
@@ -1,5 +1,6 @@
 package pl.lotto.engine;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,6 +9,8 @@ import pl.lotto.generateNumbers.Validator;
 import pl.lotto.informationForTheUser.ResultMessage;
 import pl.lotto.inputSetting.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.*;
 import java.util.stream.Stream;
 
@@ -22,18 +25,26 @@ class ApplicationEngineTest {
     private static final GenerateNumbers generateNumbersMock = mock(GenerateNumbers.class);
 
 
-    @ParameterizedTest(name = "Congratulations you hit {0} digits! Splendidly! You won the grand prize! \n" +
-            "Winning numbers were: {2} and yours were: {1}")
-
+    @ParameterizedTest(name = "Test case {index}: {3}")
     @MethodSource("provideUserNumbersAndMessages")
-    void shouldReturnCorrectMessageWhenParametersWereGiven(int result , Set<Integer> winningNumbers, Set<Integer> userNumbers, String expectedMessage) {
+    void shouldReturnCorrectMessageWhenParametersWereGiven(int result,
+                                                           Set<Integer> winningNumbers,
+                                                           Set<Integer> userNumbers,
+                                                           String expectedMessage) {
+
         // given
         mockNumbers(userNumbers, winningNumbers);
         mockResultValidator(userNumbers, winningNumbers);
+        String inputData = userNumbers.toString().replaceAll("[\\[\\]\\s]", "")
+                .replaceAll("", "\n")
+                + "\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputData.getBytes());
+
+
 
         // when
         ApplicationEngine engine = new ApplicationEngine();
-        engine.start(System.in);
+        engine.start(inputStream);
 
         // then
         assertEquals(expectedMessage, resultMessage.getMessage(result, winningNumbers, userNumbers));
@@ -41,25 +52,24 @@ class ApplicationEngineTest {
 
     private static Stream<Arguments> provideUserNumbersAndMessages() {
         final Arguments argument1 = Arguments.of(6,
-                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                Collections.unmodifiableSet(new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6))),
                 new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
                 "Congratulations you hit 6 digits! Splendidly! You won the grand prize! \n" +
                         "Winning numbers were: [1, 2, 3, 4, 5, 6] and yours were: [1, 2, 3, 4, 5, 6]");
 
-//        final Arguments argument2 = Arguments.of(4,
-//                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-//                new HashSet<>(Arrays.asList(1, 2, 3, 4, 15, 16)),
-//                "You hit 4 digits! Congratulations!\n" +
-//                        "Winning numbers were: [1, 2, 3, 4, 5, 6] and yours were: [1, 2, 3, 4, 10, 12]");
-//
-//        final Arguments argument3 = Arguments.of(1,
-//                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
-//                new HashSet<>(Arrays.asList(1, 12, 13, 14, 15, 16)),
-//                "You hit 1 , unfortunately not this time...\n" +
-//                        "Winning numbers were: [1, 2, 3, 4, 5, 6] and yours were: [1, 2, 3, 4, 10, 12]");
+        final Arguments argument2 = Arguments.of(4,
+                Collections.unmodifiableSet(new HashSet<>(Arrays.asList(1, 2, 3, 4, 15, 16))),
+                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                "You hit 4 digits! Congratulations!\n" +
+                        "Winning numbers were: [1, 2, 3, 4, 15, 16] and yours were: [1, 2, 3, 4, 5, 6]");
 
-        return Stream.of(argument1 );
-                //,argument2, argument3
+        final Arguments argument3 = Arguments.of(1,
+                Collections.unmodifiableSet(new HashSet<>(Arrays.asList(1, 12, 13, 14, 15, 16))),
+                new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                "You hit 1 , unfortunately not this time...\n" +
+                        "Winning numbers were: [1, 2, 3, 4, 15, 16] and yours were: [1, 2, 3, 4, 5, 6]");
+
+        return Stream.of(argument1, argument2, argument3);
     }
 
     private void mockNumbers(Set<Integer> userNumbers, Set<Integer> winingNumbers) {
