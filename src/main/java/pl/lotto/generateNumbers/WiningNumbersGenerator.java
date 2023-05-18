7085b883745d6355bde69b3dab3a2e7455c0746f
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-05-18 18:14:13 +0200

    refactored method generateWiningNumbers in the WiningNumbersGenerator class

--- src/main/java/pl/lotto/generateNumbers/WiningNumbersGenerator.java
+++ src/main/java/pl/lotto/generateNumbers/WiningNumbersGenerator.java
@@ -11,13 +11,11 @@ public class WiningNumbersGenerator implements GenerateNumbers {
 
     @Override
     public Set<Integer> generateWiningNumbers() {
-        Set<Integer> winingNumbersSet = new HashSet<>();
         SecureRandom secureRandom = new SecureRandom();
-        winingNumbersSet = secureRandom.ints(NumbersSetting.NUMBER_OF_NUMBERS,
+        return secureRandom.ints(NumbersSetting.NUMBER_OF_NUMBERS,
                         NumbersSetting.BOTTOM_LIMIT,
                         NumbersSetting.TOP_LIMIT)
                 .boxed()
                 .collect(Collectors.toSet());
-        return winingNumbersSet;
     }
 }
