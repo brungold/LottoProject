acb27e5f9458125fd300002caad06d61b425c9fa
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-05-19 19:07:26 +0200

    refactored method validateNumbers in the ResultValidator class

--- src/main/java/pl/lotto/generateNumbers/ResultValidator.java
+++ src/main/java/pl/lotto/generateNumbers/ResultValidator.java
@@ -1,13 +1,16 @@
 package pl.lotto.generateNumbers;
 
+import pl.lotto.informationForTheUser.ResultMessage;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class ResultValidator implements Validator {
     @Override
-    public int validateNumbers(Set<Integer> set1, Set<Integer> set2) {
-        Set<Integer> commonElements = new HashSet<>(set1);
-        commonElements.retainAll(set2);
-        return commonElements.size();
+    public int validateNumbers(Set<Integer> userNumbers, Set<Integer> winningNumbers) {
+        Set<Integer> commonElements = new HashSet<>(userNumbers);
+        commonElements.retainAll(winningNumbers);
+        int result = commonElements.size();
+        return new ResultMessage(result, userNumbers, winningNumbers);
     }
 }
