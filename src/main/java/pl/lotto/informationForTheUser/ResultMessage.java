5bf288e66059e721cc6c32d65ad859135bb219d4
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-05-19 15:57:54 +0200

    refactored methodgetMessage in the ResultMessage class

--- src/main/java/pl/lotto/informationForTheUser/ResultMessage.java
+++ src/main/java/pl/lotto/informationForTheUser/ResultMessage.java
@@ -1,44 +1,31 @@
 package pl.lotto.informationForTheUser;
 
 
+import lombok.Data;
+
 import java.util.Set;
 
 import static pl.lotto.informationForTheUser.Messages.*;
 
+@Data
 public class ResultMessage {
     private final int result;
     private final Set<Integer> userNumbersSet;
     private final Set<Integer> winningNumbers;
 
-    public ResultMessage(int result, Set<Integer> userNumbersSet, Set<Integer> winningNumbers) {
-        this.result = result;
-        this.userNumbersSet = userNumbersSet;
-        this.winningNumbers = winningNumbers;
-    }
 
-    private int getResult() {
-        return result;
-    }
 
-    private Set<Integer> getUserNumbersSet() {
-        return userNumbersSet;
-    }
-
-    private Set<Integer> getWinningNumbers() {
-        return winningNumbers;
-    }
-
-    public String getMessage(int result, Set<Integer> userNumbersSet, Set<Integer> winningNumber) {
-        switch (getResult()) {
+    public String getMessage() {
+        switch (result) {
             case 6:
-                return String.format(WINNING_MESSAGE, getResult(), setToString(getWinningNumbers()), setToString(getUserNumbersSet()));
+                return String.format(WINNING_MESSAGE, result, setToString(getWinningNumbers()), setToString(getUserNumbersSet()));
 
             case 5:
             case 4:
-                return String.format(HIT_FOUR_OR_FIVE, getResult(), setToString(getWinningNumbers()), setToString(getUserNumbersSet()));
+                return String.format(HIT_FOUR_OR_FIVE, result, setToString(getWinningNumbers()), setToString(getUserNumbersSet()));
 
             default:
-                return String.format(LESS_HITS_THAN_FOUR, getResult(), setToString(getWinningNumbers()), setToString(getUserNumbersSet()));
+                return String.format(LESS_HITS_THAN_FOUR, result, setToString(getWinningNumbers()), setToString(getUserNumbersSet()));
         }
     }
 
