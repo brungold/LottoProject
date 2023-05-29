e639c4d1096771f440c3bcf8f16b23eab9a46e18
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-05-29 22:44:51 +0200

    refactored the class ResultValidator & interface Validator

--- src/main/java/pl/lotto/generateNumbers/ResultValidator.java
+++ src/main/java/pl/lotto/generateNumbers/ResultValidator.java
@@ -7,10 +7,10 @@ import java.util.Set;
 
 public class ResultValidator implements Validator {
     @Override
-    public ResultMessage validateNumbers(Set<Integer> userNumbers, Set<Integer> winningNumbers) {
+    public int validateNumbers(Set<Integer> userNumbers, Set<Integer> winningNumbers) {
         Set<Integer> commonElements = new HashSet<>(userNumbers);
         commonElements.retainAll(winningNumbers);
         int result = commonElements.size();
-        return new ResultMessage(result, userNumbers, winningNumbers);
+        return result;
     }
 }
--- src/main/java/pl/lotto/generateNumbers/Validator.java
+++ src/main/java/pl/lotto/generateNumbers/Validator.java
@@ -5,5 +5,5 @@ import pl.lotto.informationForTheUser.ResultMessage;
 import java.util.Set;
 
 public interface Validator {
-    ResultMessage validateNumbers(Set<Integer> set1, Set<Integer> set2);
+    int validateNumbers(Set<Integer> set1, Set<Integer> set2);
 }
