25ac3e92e39a634b4336c19ede7f4d03d0327a69
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-05-31 00:20:11 +0200

    added the test case to UserNumbersSetTest

--- src/test/java/pl/lotto/inputSetting/UserNumbersSetTest.java
+++ src/test/java/pl/lotto/inputSetting/UserNumbersSetTest.java
@@ -58,15 +58,16 @@ class UserNumbersSetTest implements SampleScanner {
     }
 
     @Test
-    public void collect_numbers_returns_correct_set_of_numbers_when_user_inputs_both_valid_and_invalid_data() {
+    public void should_return_correct_set_of_numbers_when_user_inputs_both_valid_and_invalid_data() {
         //given
-        String inputData = "1\n2\n3\n10\n10\n6\n100\n99\n";
-        UserNumbersSet userNumbersSet = createUserNumbersSet(inputData);
+        Set<Integer> expectedNumbers = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(1,2,3,10,99,33)));
+        String inputData = "1\n2\n3\n10\npięć\n\n10\n6\n100\n99\n\n33\n";
+        Scanner userNumbers = createSampleScanner(inputData);
 
         //when
-        Set<Integer> selectedNumbers = userNumbersSet.collectNumbers();
+        final Set<Integer> selectedNumbers = userNumbersSet.collectUserNumbers(userNumbers);
 
         //then
-        assertEquals(6, selectedNumbers.size());
+        assertThat(expectedNumbers).isEqualTo(selectedNumbers);
     }
 }
