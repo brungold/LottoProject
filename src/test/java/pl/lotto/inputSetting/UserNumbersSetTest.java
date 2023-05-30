324fd384d3b97f01b5a3b95b59e0eed7d4ff129e
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-05-31 00:13:22 +0200

    added two more test cases to UserNumbersSetTest

--- src/test/java/pl/lotto/inputSetting/UserNumbersSetTest.java
+++ src/test/java/pl/lotto/inputSetting/UserNumbersSetTest.java
@@ -16,7 +16,7 @@ class UserNumbersSetTest implements SampleScanner {
     UserNumbersSet userNumbersSet = new UserNumbersSet();
 
     @Test
-    public void test_collect_numbers() {
+    public void should_return_numbers_in_set_when_all_in_range_1_to_99() {
         // given
         Set<Integer> expectedNumbers = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(1,2,3,4,5,6)));
         String inputData = "1\n2\n3\n4\n5\n6\n";
@@ -30,30 +30,31 @@ class UserNumbersSetTest implements SampleScanner {
     }
 
     @Test
-    public void collect_numbers_returns_correct_set_of_numbers_when_user_inputs_valid_data() {
-        //given
-        String inputData = "10\n24\n15\n23\n18\n99\n";
-        UserNumbersSet userNumbersSet = createUserNumbersSet(inputData);
+    public void should_return_numbers_in_set_when_one_not_in_range() {
+        // given
+        Set<Integer> expectedNumbers = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(1,2,3,4,5,6)));
+        String inputData = "1\n2\n3\n4\n5\n100\n6\n";
+        Scanner userNumbers = createSampleScanner(inputData);
 
-        //when
-        Set<Integer> selectedNumbers = userNumbersSet.collectNumbers();
+        // when
+        final Set<Integer> selectedNumbers = userNumbersSet.collectUserNumbers(userNumbers);
 
-        //then
-        Set<Integer> expectedNumbers = new HashSet<>(Arrays.asList(99, 18, 23, 15, 24, 10));
-        assertEquals(expectedNumbers, selectedNumbers);
+        // then
+        assertThat(expectedNumbers).isEqualTo(selectedNumbers);
     }
-
     @Test
-    public void negative_test_collect_numbers_returns_error_when_user_inputs_less_than_6_numbers() {
-        //given
-        String inputData = "1\n2\n3\n4\n5\n";
-        UserNumbersSet userNumbersSet = createUserNumbersSet(inputData);
+    void should_return_message_wrong_number_because_of_provided_string() {
+        // given
+        String expectedMessage = "Upsss, please check if this is the correct value.\n";
+        String inputData = "pięć\n";
+        Scanner userNumbers = createSampleScanner(inputData);
 
-        //when
-        Set<Integer> selectedNumbers = userNumbersSet.collectNumbers();
+        // when
+        final Set<Integer> selectedNumbers = userNumbersSet.collectUserNumbers(userNumbers);
 
-        //then
-        assertNotEquals(6, selectedNumbers.size());
+        // then
+        assertTrue(selectedNumbers.isEmpty());
+        assertEquals(expectedMessage, Messages.WRONG_NUMBER);
     }
 
     @Test
@@ -67,52 +68,5 @@ class UserNumbersSetTest implements SampleScanner {
 
         //then
         assertEquals(6, selectedNumbers.size());
-    }
-
-    /*
-    Below are two tests that are expected to fail because we do not provide 6 correct digits
-     */
-    @Test
-    public void negative_test_collect_numbers_returns_error_when_user_inputs_number_outside_1_to_99_range() {
-        //given
-        String inputData = "1\n2\n3\n100\n5\n6\n";
-        UserNumbersSet userNumbersSet = createUserNumbersSet(inputData);
-
-        //when
-        Set<Integer> selectedNumbers = userNumbersSet.collectNumbers();
-
-        //then
-        assertNotEquals(5, selectedNumbers.size());
-    }
-
-    @Test
-    public void negative_test_collect_numbers_returns_error_when_user_inputs_duplicate_number() {
-        //given
-        String inputData = "1\n2\n3\n10\n10\n6\n";
-        UserNumbersSet userNumbersSet = createUserNumbersSet(inputData);
-
-        //when
-        Set<Integer> selectedNumbers = userNumbersSet.collectNumbers();
-
-        //then
-        assertEquals(5, selectedNumbers.size());
-    }
-
-    @Test
-    void test_collect_numbers_wrong_number() {
-        // given
-        String inputData = "pięć\n";
-        UserNumbersSet userNumbersSet = createUserNumbersSet(inputData);
-
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        // when
-        userNumbersSet.collectNumbers();
-
-        // then
-        String expectedMessage = "Upsss, please check if this is the correct value.\n";
-        assertEquals(expectedMessage, Messages.WRONG_NUMBER);
-        assertEquals("You must enter 6 digits from 1 to 99. Press the ENTER button after each digit\npięć\nUpsss, please check if this is the correct value.\n", outContent.toString());
     }
 }
