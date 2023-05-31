036aab42c485de19e6fd214e0ccd7ffe01a71bc0
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-05-31 09:57:17 +0200

    refactored the ResultMessageTest class

--- src/test/java/pl/lotto/informationForTheUser/ResultMessageTest.java
+++ src/test/java/pl/lotto/informationForTheUser/ResultMessageTest.java
@@ -2,7 +2,6 @@ package pl.lotto.informationForTheUser;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
